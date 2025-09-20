5534a6430dd7243d9536d5ae0af388421539714e
elitekbtu <satbaldievst@mail.ru>
2025-09-20 18:15:19 +0500

    styling

---
+++ frontend/src/pages/BackgroundPathsDemo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { DemoBackgroundPaths } from '@/components/ui/demo';
+
+const BackgroundPathsDemo: React.FC = () => {
+  return <DemoBackgroundPaths />;
+};
+
+export default BackgroundPathsDemo;
